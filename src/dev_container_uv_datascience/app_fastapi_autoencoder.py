88159e5204d72c1ddba0d23e4d59a92603cd9efe
SolidData <david.tiefenthaler@web.de>
2025-02-03 17:39:57 +0000

    refactor: minimum example to reproduce the permission error

--- src/dev_container_uv_datascience/app_fastapi_autoencoder.py
+++ src/dev_container_uv_datascience/app_fastapi_autoencoder.py
@@ -1,25 +1,7 @@
-import os
-from typing import Any
-
 import uvicorn
-from fastapi import FastAPI, HTTPException, Response
-from pydantic import BaseModel, conint
-from torch import rand
-
-from .lit_auto_encoder import LitAutoEncoder
-from .train_autoencoder import train_litautoencoder
+from fastapi import FastAPI, Response
 
 app = FastAPI()
-
-# Global variables for encoder/decoder
-encoder = None
-decoder = None
-is_model_trained = False  # Track model training status
-
-
-# Input validation model
-class NumberFakeImages(BaseModel):
-    n_fake_images: conint(ge=1, le=10)  # type: ignore # Between 1 and 10 fake images allowed
 
 
 # ROOT endpoint
@@ -28,70 +10,9 @@ def read_root() -> Response:
     """Root endpoint that provides information about the API."""
 
     message = """
-    ⚡⚡⚡ Welcome to the LitAutoEncoder API! ⚡⚡⚡
-    - To train the model, send a POST request to '/train' without providing any additional input.
-    - To get encodings for random fake images, POST to '/embed' with JSON input: 
-      {'n_fake_images': [1-10]} in the request body.
+    ⚡⚡⚡ Welcome to the FastAPI API! ⚡⚡⚡
     """
     return Response(content=message, media_type="text/plain")
-
-
-# POST endpoint to train the autoencoder
-@app.post("/train")
-def train_model() -> dict[str, str]:
-    """Train the autoencoder model.
-
-    Returns:
-        dict[str, str]: A message indicating the training status.
-    """
-    global encoder, decoder, is_model_trained
-
-    if is_model_trained:
-        return {"message": "Model is already trained."}
-
-    encoder, decoder, is_model_trained = train_litautoencoder()
-    return {"message": "Model training completed successfully."}
-
-
-# POST endpoint to embed fake images using the trained autoencoder
-@app.post("/embed")
-def embed(input_data: NumberFakeImages) -> dict[str, Any]:
-    """Embed fake images using the trained autoencoder.
-
-    Args:
-        input_data (NumberFakeImages): Input data containing the number of fake images to embed.
-
-    Returns:
-        dict[str, Any]: A dictionary containing the embeddings of the fake images.
-    """
-    global encoder, decoder
-
-    if encoder is None or decoder is None:
-        raise HTTPException(
-            status_code=500, detail="Model not initialized. Train the model first."
-        )
-
-    n_fake_images = input_data.n_fake_images
-    checkpoint_path = "./lightning_logs/version_0/checkpoints/epoch=0-step=100.ckpt"
-
-    if not os.path.exists(checkpoint_path):
-        raise HTTPException(
-            status_code=500, detail="Checkpoint file not found. Train the model first."
-        )
-
-    # Load the trained autoencoder from the checkpoint
-    autoencoder = LitAutoEncoder.load_from_checkpoint(
-        checkpoint_path, encoder=encoder, decoder=decoder
-    )
-    encoder_model = autoencoder.encoder
-    encoder_model.eval()
-
-    # Generate fake image embeddings based on user input
-    fake_image_batch = rand(n_fake_images, 28 * 28, device=autoencoder.device)
-    embeddings = encoder_model(fake_image_batch)
-    # print("⚡" * 20, "\nPredictions (image embeddings):\n", embeddings, "\n", "⚡" * 20)
-
-    return {"embeddings": embeddings.tolist()}
 
 
 # Application entry point
--- tests/test_train_autoencoder.py
+++
@@ -1,8 +0,0 @@
-from dev_container_uv_datascience.train_autoencoder import train_litautoencoder
-
-
-def test_train_litautoencoder() -> None:
-    encoder, decoder, is_model_trained = train_litautoencoder()
-    assert encoder is not None
-    assert decoder is not None
-    assert is_model_trained is True
